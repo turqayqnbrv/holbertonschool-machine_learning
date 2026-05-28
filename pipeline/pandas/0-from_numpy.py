a7e8d0abc5185a9aeadd0b2739f423cbd1af275a
turqayqnbrv <128743545+turqayqnbrv@users.noreply.github.com>
2026-05-28 16:44:26 +0400

    Refactor docstring and formatting in from_numpy function

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -8,7 +8,9 @@ import pandas as pd
 
 
 def from_numpy(array):
-    alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", 
+                "K", "L", "M", "N", "O", "P", "Q", 
+                "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
     
     column_labels = []
     for i in range(len(array[0])):
