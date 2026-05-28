e415979f973f298a2586824bb0278c620152266e
turqayqnbrv <128743545+turqayqnbrv@users.noreply.github.com>
2026-05-28 16:51:57 +0400

    Implement from_file function for loading DataFrame
    Added a new from_file function to load data from a CSV file into a DataFrame.

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -7,7 +7,19 @@ New code updates Pandas DataFrame script to:
 """
 
 import pandas as pd
-from_file = __import__('2-from_file').from_file
+def from_file(filename, delimiter):
+    """
+    Loads data from a file as a Pandas DataFrame
+
+    parameters:
+        filename [str]: file to load the data from
+        delimiter [str]: the column separator
+
+    returns:
+        the newly created pd.DataFrame
+    """
+    df = pd.read_csv(filename, delimiter=delimiter)
+    return df
 
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
