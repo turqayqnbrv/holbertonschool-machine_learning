15a42d8752f51603a92de3ba3fcd9e2faf222b9f
turqayqnbrv <128743545+turqayqnbrv@users.noreply.github.com>
2026-05-28 16:45:00 +0400

    Enhance docstring for from_numpy function
    Updated docstring to provide detailed parameter and return information for the from_numpy function.

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -8,10 +8,21 @@ import pandas as pd
 
 
 def from_numpy(array):
-    alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", 
-                "K", "L", "M", "N", "O", "P", "Q", 
-                "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-    
+    """
+    Creates a Pandas DataFrame from a numpy.ndarray
+
+    parameters:
+        array [numpy.ndarray]: array to create pd.DataFrame from
+
+    columns of the DataFrame should be labeled in alphabetical order
+        and capitalized (there will not be more than 26 columns)
+
+    returns:
+        the newly created pd.DataFrame
+    """
+    alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I",
+                "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+                "S", "T", "U", "V", "W", "X", "Y", "Z"]
     column_labels = []
     for i in range(len(array[0])):
         column_labels.append(alphabet[i])
