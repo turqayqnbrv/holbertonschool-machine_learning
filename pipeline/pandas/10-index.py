490ed289cda21d64fb5ef4d75b2a8298a0835ebb
turqayqnbrv <128743545+turqayqnbrv@users.noreply.github.com>
2026-05-31 11:54:43 +0400

    Implement index function for DataFrame
    Added a function to set the Timestamp column as the index for a DataFrame.

--- pipeline/pandas/10-index.py
+++ pipeline/pandas/10-index.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 """
-New code indexes the DataFrame on the Timestamp column
+Module containing the index function for pandas DataFrame
 """
 
-import pandas as pd
-from_file = __import__('2-from_file').from_file
 
-df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+def index(df):
+    """
+    Sets the Timestamp column as the DataFrame index.
 
-df = df.set_index('Timestamp')
+    Parameters:
+        df (pd.DataFrame): The input DataFrame.
 
-print(df.tail())
+    Returns:
+        pd.DataFrame: The indexed DataFrame.
+    """
+    return df.set_index('Timestamp')
