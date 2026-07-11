ff053f8fd23b5b39f469dedd8c14e07acb54285a
turqayqnbrv <128743545+turqayqnbrv@users.noreply.github.com>
2026-07-11 23:56:26 +0400

    Add files via upload

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-"""Script to train a model using keras"""
+"""
+Defines a function that trains a model using mini-batch gradient descent
+using Keras library
+"""
+
 
 import tensorflow.keras as K
 
@@ -7,22 +11,29 @@ import tensorflow.keras as K
 def train_model(network, data, labels, batch_size, epochs,
                 verbose=True, shuffle=False):
     """
-    Function to train a model using keras
-    Args:
-        network: model to train
-        data: numpy.ndarray of shape (m, nx) containing the input data
-        labels: one-hot numpy.ndarray of shape (m, classes) containing
-                the labels of data
-        batch_size: size of the batch used for mini-batch gradient descent
-        epochs: number of passes through data for mini-batch gradient descent
-        verbose: boolean that determines if output should be printed during
-                 training
-        shuffle: boolean that determines whether to shuffle the batches every
-                 epoch.
-    Returns: History object generated after training the model
+    Trains a model using mini-batch gradient descent with Keras
 
+    parameters:
+        network [keras model]: model to train
+        data [numpy.ndarray of shape (m, nx)]:
+            contains the input data
+        labels [one-hot numpy.ndarray of shape (m, classes)]:
+            contains labels of data
+        batch_size [int]:
+            size of batch used for mini-batch gradient descent
+        epochs [int]:
+            number of passes through data for mini-batch gradient descent
+        verbose [boolean]:
+            determines if output should be printed during training
+        shuffle [boolean]:
+            determines whether to shuffle the batches every epoch
+
+    returns:
+        the History object generated after training the model
     """
-    history = network.fit(data, labels, epochs=epochs,
-                          batch_size=batch_size, verbose=verbose,
+    history = network.fit(x=data, y=labels,
+                          batch_size=batch_size,
+                          epochs=epochs,
+                          verbose=verbose,
                           shuffle=shuffle)
     return history
--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-"""Script to train a model using keras"""
+"""
+Updates function that trains a model using mini-batch gradient descent
+to also analyze validation data using Keras library
+"""
+
 
 import tensorflow.keras as K
 
@@ -7,29 +11,33 @@ import tensorflow.keras as K
 def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, verbose=True, shuffle=False):
     """
-    Function to train a model using keras and validate data
-    Args:
-        network: model to train
-        data: numpy.ndarray of shape (m, nx) containing the input data
-        labels: one-hot numpy.ndarray of shape (m, classes) containing
-                the labels of data
-        batch_size: size of the batch used for mini-batch gradient descent
-        epochs: number of passes through data for mini-batch gradient descent
-        validation_data: data to validate the model with, if not None
-        verbose: boolean that determines if output should be printed during
-                 training
-        shuffle: boolean that determines whether to shuffle the batches every
-                 epoch.
-    Returns: History object generated after training the model
+    Trains a model using mini-batch gradient descent,
+        including analyzing validation data
 
+    parameters:
+        network [keras model]: model to train
+        data [numpy.ndarray of shape (m, nx)]:
+            contains the input data
+        labels [one-hot numpy.ndarray of shape (m, classes)]:
+            contains labels of data
+        batch_size [int]:
+            size of batch used for mini-batch gradient descent
+        epochs [int]:
+            number of passes through data for mini-batch gradient descent
+        validation_data:
+            data to be analyzed during model training
+        verbose [boolean]:
+            determines if output should be printed during training
+        shuffle [boolean]:
+            determines whether to shuffle the batches every epoch
+
+    returns:
+        the History object generated after training the model
     """
-    if validation_data:
-        validation_data = validation_data
-    else:
-        validation_data = None
-
-    return network.fit(x=data, y=labels, batch_size=batch_size,
-                       epochs=epochs,
-                       validation_data=validation_data,
-                       verbose=verbose,
-                       shuffle=shuffle)
+    history = network.fit(x=data, y=labels,
+                          batch_size=batch_size,
+                          epochs=epochs,
+                          validation_data=validation_data,
+                          verbose=verbose,
+                          shuffle=shuffle)
+    return history
