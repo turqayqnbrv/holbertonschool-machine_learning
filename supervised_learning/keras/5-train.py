34f5ded0d169dbd183d94cdee2d8f1b2b2ed5750
turqayqnbrv <128743545+turqayqnbrv@users.noreply.github.com>
2026-07-12 00:00:32 +0400

    Update 5-train.py

--- supervised_learning/keras/5-train.py
+++ supervised_learning/keras/5-train.py
@@ -34,7 +34,8 @@ def train_model(network, data, labels, batch_size, epochs,
     returns:
         the History object generated after training the model
     """
-    history = network.fit(x=data, y=labels,
+    history = network.fit(x=data,
+                          y=labels,
                           batch_size=batch_size,
                           epochs=epochs,
                           validation_data=validation_data,
